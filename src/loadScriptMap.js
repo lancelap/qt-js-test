fd81a3c67bfe38a1a405c9023e76b54ba1f64316
lancelap <lancelap@lancelaps-iMac.local>
2018-10-10 21:23:22 +0500

    add loadScriptMap

---
+++ src/loadScriptMap.js
@@ -0,0 +1,16 @@
+export default function loadScriptMap(url) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script'); 
+    const body = document.getElementsByTagName('body')[0];
+    
+    if(url.match(/onload=initYandexMap/)) {
+      window.initYandexMap = () => resolve();
+    } else {
+      reject(new Error("require onload func"))
+    }
+
+    script.src = url;
+    script.onerror = reject;
+    body.appendChild(script);
+  })
+}
