593fc1372be7cc46700579c84c7dd416ea91b854
lancelap <lancelap@lancelaps-iMac.local>
2018-10-12 11:00:52 +0500

    update component Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import loadScriptMap from '../loadScriptMap';
+import YandexMap from './YandexMap';
 
 class Map extends Component {
   constructor() {
@@ -8,38 +9,32 @@ class Map extends Component {
     this.state = {
       map: null,
       loaded: false,
-      YMapsID: 'YMapsID'
+      mapId: 'YMapsID'
     };
   }
-
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   console.log( '___' , this.props, nextProps, '___', this.state, nextState)
-  //   return true
-  // }
 
   componentDidMount() {
     loadScriptMap('https://api-maps.yandex.ru/2.1/?lang=ru_RU&onload=initYandexMap')
       .then((ymaps) => {
-        const mapY = new ymaps.Map("YMapsID", {
-          center: [55.87, 37.66],
-          zoom: 10
-        });
-
-        this.setState({
-          map: mapY,
-          loaded: true
-        })
+        this.initMap(ymaps);
       })
       .catch(error => console.error(error));
   }
 
+  initMap(ymaps) {
+    const mapY = new ymaps.Map(this.state.mapId, {
+      center: [55.87, 37.66],
+      zoom: 10
+    });
+
+    this.setState({
+      map: mapY,
+      loaded: true
+    })
+  }
+
   render() {
-    return (
-      <div>
-        <h1>Map</h1>
-        <div id="YMapsID" style={{backgroundColor: 'red', width: '450px', height: '350px'}}></div>  
-      </div>
-    )
+    return <YandexMap id={this.state.mapId}/>
   }
 
 }
