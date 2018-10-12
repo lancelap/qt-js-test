17365b952b7c8b733dbbccc8ea3ec999431a97e1
lancelap <lancelap@lancelaps-iMac.local>
2018-10-12 11:53:25 +0500

    add MarkerForm

---
+++ src/components/MarkerForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class MarkerForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+      text: ''
+    }
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.onSubmit}>
+        <input style={{width: '196px'}}
+          type="text"
+          value={this.state.text}
+          placeholder="Новая точка маршрута" 
+          onChange={this.onChangeMarkerTitle} />
+      </form>
+    )
+  }
+
+  onChangeMarkerTitle = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.addMarker(Date.now(), this.state.text);
+    this.setState({text: ''})
+  }
+}
+
+export default MarkerForm;
