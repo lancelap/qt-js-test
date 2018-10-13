ec0822a3bc843b5f6c60894f1dd23c998e19bbda
lancelap <lancelap@lancelaps-iMac.local>
2018-10-13 21:05:32 +0500

    new Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import loadScriptMap from '../loadScriptMap';
 import YandexMap from './YandexMap';
+import MarkerList from './MarkerList';
+import MarkerForm from './MarkerForm';
+
 
 class Map extends Component {
   constructor() {
@@ -8,8 +11,10 @@ class Map extends Component {
 
     this.state = {
       map: null,
+      ymaps: null,
       loaded: false,
-      mapId: 'YMapsID'
+      mapId: 'YMapsID',
+      markers: {},
     };
   }
 
@@ -29,12 +34,70 @@ class Map extends Component {
 
     this.setState({
       map: mapY,
+      ymaps: ymaps,
       loaded: true
     })
   }
 
   render() {
-    return <YandexMap id={this.state.mapId}/>
+    const {mapId, map, ymaps, loaded, markers} = this.state;
+    return (
+      <div>
+        <MarkerForm addMarker={this.addMarker} loaded={loaded} />
+
+        {loaded ? (
+          <MarkerList 
+            map={map} 
+            deleteMarker={this.deleteMarker} 
+            addDragEndListener={this.addDragEndListener}
+            removeDragEndListener={this.removeDragEndListener}
+            ymaps={ymaps} 
+            markers={markers} />
+        ) : (
+          null
+        )}
+        
+        <YandexMap id={mapId} />
+      </div>
+    )
+  }
+
+  addMarker = (id, text) => {
+    const {map, ymaps} = this.state;
+    const coordinates = map.getCenter();
+    const placemark = new ymaps.Placemark(coordinates, {balloonContent: text}, { 
+      draggable: true 
+    });
+
+    map.geoObjects.add(placemark); 
+    const marker = {timestamp: id, text, coordinates, placemark};
+    this.setState({markers: {...this.state.markers, [id]: marker}});
+  }
+
+  deleteMarker = (id) => {
+    const newMarkers = {...this.state.markers};
+    this.state.map.geoObjects.remove(newMarkers[id].placemark);
+    delete newMarkers[id];
+
+    this.setState({markers: {...newMarkers}})
+  }
+
+  addDragEndListener = (id) => {
+    const markers = this.state.markers;
+    const marker = markers[id].placemark;
+
+    marker.events.add('dragend', () => {
+      const newMarkers = {...this.state.markers};
+      newMarkers[id].coordinates = marker.geometry.getCoordinates();
+      this.setState({markers: {...newMarkers}})
+    });
+  }
+
+  removeDragEndListener = (id) => {
+    const markers = this.state.markers;
+    const marker = markers[id].placemark;
+    
+    marker.events.remove('dragend');
   }
 
 }
