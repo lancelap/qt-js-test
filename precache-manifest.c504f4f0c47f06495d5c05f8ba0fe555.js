1e31879d11b816ab8886111433f9f6a610f66960
lancelap <lancelap@lancelaps-iMac.local>
2018-10-16 12:48:17 +0500

    Updates

---
+++ precache-manifest.c504f4f0c47f06495d5c05f8ba0fe555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dd60d3464ef0f5e44c19",
+    "url": "/static/js/main.dd60d346.chunk.js"
+  },
+  {
+    "revision": "26595268a47bbeb8412b",
+    "url": "/static/js/1.26595268.chunk.js"
+  },
+  {
+    "revision": "dd60d3464ef0f5e44c19",
+    "url": "/static/css/main.94dc060f.chunk.css"
+  },
+  {
+    "revision": "7430ec9124facaa8013afe3c4440eb5d",
+    "url": "/index.html"
+  }
+];
