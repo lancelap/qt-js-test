3c7323fc4136e5caa3ef14752ac633dd55726348
lancelap <lancelap@lancelaps-iMac.local>
2018-10-19 21:01:17 +0500

    change  Marker

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,7 +1,65 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import {DragSource, DropTarget} from 'react-dnd';
+import flow from 'lodash/flow';
+
+const cardSource = {
+  beginDrag(props) {
+    return {
+      id: props.id,
+      index: props.index,
+    };
+  },
+};
+
+const cardTarget = {
+  hover(props, monitor, component) {
+    const dragIndex = monitor.getItem().index;
+    const hoverIndex = props.index;
+
+    // Don't replace items with themselves
+    if (dragIndex === hoverIndex) {
+      return;
+    }
+
+    // Time to actually perform the action
+    props.moveMarker(dragIndex, hoverIndex);
+
+    // Note: we're mutating the monitor item here!
+    // Generally it's better to avoid mutations,
+    // but it's good here for the sake of performance
+    // to avoid expensive index searches.
+    monitor.getItem().index = hoverIndex;
+  },
+};
 
 class Marker extends Component {
+  componentDidMount() {
+    const {addDragEndListener, text, id, getMapY} = this.props;
+    const {ymaps, mapY} = getMapY();
+    const coordinates = mapY.getCenter();
+
+    const placemark = new ymaps.Placemark(coordinates, {
+      balloonContent: this.props.text
+    }, { 
+      draggable: true 
+    });
+
+    this.placemark = placemark;
+    
+    mapY.geoObjects.add(placemark); 
+
+    placemark.events.add('dragend', () => {
+      const coordinates = placemark.geometry.getCoordinates();
+      addDragEndListener(this.props.index, {timestamp: id, text, coordinates});
+    });
+  }
+
+  componentWillUnmount() {
+    this.placemark.events.remove('dragend');
+    this.props.getMapY().mapY.geoObjects.remove(this.placemark);
+  }
+
   render() {
     const {
       text, 
@@ -32,11 +90,23 @@ class Marker extends Component {
   }
 }
 
+export default flow(
+  DragSource(
+    'marker',
+    cardSource,
+    (connect, monitor) => ({
+      connectDragSource: connect.dragSource(),
+      isDragging: monitor.isDragging(),
+    }),
+  ),
+  DropTarget('marker', cardTarget, (connect) => ({
+    connectDropTarget: connect.dropTarget(),
+  }))
+)(Marker);
+
 Marker.propTypes = {
   id: PropTypes.number,
   deleteMarker: PropTypes.func, 
   addDragEndListener: PropTypes.func, 
   getMapY: PropTypes.func
 };
-
-export default Marker;
