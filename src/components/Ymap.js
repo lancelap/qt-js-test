755cf40a4be82a36b89753ffc9e550e90ed67abf
lancelap <lancelap@lancelaps-iMac.local>
2018-10-19 21:02:55 +0500

    change Ymap

--- src/components/Ymap.js
+++ src/components/Ymap.js
@@ -1,81 +1,57 @@
 import React, { Component } from 'react';
+import loadScriptMap from '.././loadScriptMap';
 import MarkerList from './MarkerList';
-import MarkerForm from './MarkerForm';
-import update from 'immutability-helper';
 import Loader from './Loader';
+import YandexMap from './YandexMap';
 
 class Ymap extends Component {
   constructor() {
     super();
-
+    this.getMapY = null;
     this.state = {
-      markers: [],
+      loaded: false,
+      mapId: 'YMapsID'
     };
   }
 
+  componentDidMount() {
+    loadScriptMap('https://api-maps.yandex.ru/2.1/?lang=ru_RU&onload=initYandexMap')
+      .then((ymaps) => {
+        this.initMap(ymaps);
+      })
+      .catch(error => console.error(error));
+  }
+
   render() {
-    const {loaded, getMapY} = this.props;
-    const {markers} = this.state;
-    return (
-      <div>
+    const {loaded, mapId} = this.state;
+    return (      
+      <div className='map'>
         {loaded ? (
-          <div>
-            <MarkerForm addMarker={this.addMarker} getMapY={getMapY} />
-            <MarkerList 
-              getMapY={getMapY} 
-              deleteMarker={this.deleteMarker} 
-              moveMarker={this.moveMarker}
-              addDragEndListener={this.addDragEndListener}
-              removeDragEndListener={this.removeDragEndListener}
-              markers={markers} />
-          </div>
+          <MarkerList getMapY={this.getMapY} />
         ) : (
           <Loader />
         )}
-      </div>
-    );
+        <YandexMap id={mapId} />
+      </div>);
   }
 
-  addMarker = (id, text, coordinates) => {
-    const marker = {timestamp: id, text, coordinates};
+  initMap(ymaps) {
+    const mapY = new ymaps.Map(this.state.mapId, {
+      center: [55.87, 37.66],
+      zoom: 10
+    });
+
+    this.getMapY = function() {
+      return {
+        ymaps,
+        mapY
+      };
+    };
+
     this.setState({
-      markers: [...this.state.markers, marker]
+      loaded: true
     });
   }
-
-  deleteMarker = (index) => {
-    this.setState(
-      update(this.state, {
-        markers: {
-          $splice: [[index, 1]]
-        }
-      })
-    );
-  }
-
-  addDragEndListener = (index, placemark) => {
-    this.setState(
-      update(this.state, {
-        markers: {
-          $splice: [[index, 1, placemark]]
-        }
-      })
-    );
-  }
-
-  moveMarker = (dragIndex, hoverIndex) => {
-    const {markers} = this.state;
-    const dragCard = markers[dragIndex];
-
-    this.setState(
-      update(this.state, {
-        markers: {
-          $splice: [[dragIndex, 1], [hoverIndex, 0, dragCard]],
-        },
-      }),
-    );
-  }
-
 }
 
 export default Ymap;
