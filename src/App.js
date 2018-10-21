1511ed64698487971f8adf3eb296a5dd6920905f
lacelap <lancelap@mail.ru>
2018-10-21 14:04:32 +0500

    remove Ymap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
-import Ymap from './components/Ymap';
+import loadScriptMap from './loadScriptMap';
+import MarkerList from './components/MarkerList';
+import Loader from './components/Loader';
+import YandexMap from './components/YandexMap';
 
-class App extends Component {
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loaded: false,
+      mapId: 'YMapsID',
+      mapY: null,
+      ymaps: null
+    };
+  }
+
+  componentDidMount() {
+    loadScriptMap('https://api-maps.yandex.ru/2.1/?lang=ru_RU&onload=initYandexMap')
+      .then((ymaps) => {
+        this.initMap(ymaps);
+      })
+      .catch(error => console.error(error));
+  }
+
   render() {
-    return <Ymap />;
+    const {loaded, mapId, ymaps, mapY} = this.state;
+    return (      
+      <div className='map'>
+        {loaded ? (
+          <MarkerList ymaps={ymaps} mapY={mapY}/>
+        ) : (
+          <Loader />
+        )}
+        <YandexMap id={mapId} />
+      </div>);
+  }
+
+  initMap(ymaps) {
+    const mapY = new ymaps.Map(this.state.mapId, {
+      center: [55.87, 37.66],
+      zoom: 10
+    });
+
+    this.setState({
+      loaded: true,
+      mapY: mapY,
+      ymaps: ymaps
+    });
   }
 }
-
 export default DragDropContext(HTML5Backend)(App);
--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import {mount} from 'enzyme';
-import App from '../App';
-import Ymap from '../components/Ymap';
+import {shallow} from 'enzyme';
+import {App} from '../App';
+import Loader from '../components/Loader';
+import MarkerList from '../components/MarkerList';
+import YandexMap from '../components/YandexMap';
+jest.mock('.././loadScriptMap');
 
-it('should render Ymap', () => {
-  const container = mount(<App />);
-  expect(container.find(Ymap)).toHaveLength(1);
+it('should render Loader, YandexMap', () => {
+  const container = shallow(<App />);
+  expect(container.contains(<Loader />)).toEqual(true);
+  expect(container.contains(<YandexMap id={container.state().mapId} />)).toEqual(true);
+  expect(container.find(MarkerList)).toHaveLength(0);
+});
+
+it('should render MarkerList, YandexMap', () => {
+  const container = shallow(<App />);
+  process.nextTick(() => {
+    expect(container.contains(<Loader />)).toEqual(false);
+    expect(container.find(MarkerList)).toHaveLength(1);
+    expect(container.contains(<YandexMap id={container.state().mapId} />)).toEqual(true);
+  });
 });
