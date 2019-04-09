dc422e8a115f2f59392698eea6307fbd73e6c092
Daniil Potapov <potapovdg@pik.ru>
2019-04-09 18:49:30 +0300

    fix test

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import * as React from 'react';
 import {shallow} from 'enzyme';
 import {App} from '../App';
 import Loader from '../components/Loader';
-import MarkerList from '../components/MarkerList';
 import YandexMap from '../components/YandexMap';
 jest.mock('.././loadScriptMap');
 
@@ -10,14 +9,14 @@ it('should render Loader, YandexMap', () => {
   const container = shallow(<App />);
   expect(container.contains(<Loader />)).toEqual(true);
   expect(container.contains(<YandexMap id={container.state().mapId} />)).toEqual(true);
-  expect(container.find(MarkerList)).toHaveLength(0);
+  expect(container.find('MarkerList').length).toBe(0);
 });
 
 it('should render MarkerList, YandexMap', () => {
   const container = shallow(<App />);
   process.nextTick(() => {
     expect(container.contains(<Loader />)).toEqual(false);
-    expect(container.find(MarkerList)).toHaveLength(1);
+    expect(container.find('MarkerList').length).toBe(1);
     expect(container.contains(<YandexMap id={container.state().mapId} />)).toEqual(true);
   });
 });
